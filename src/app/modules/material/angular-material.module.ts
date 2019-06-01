c6d1b64eb5d2ad7a0c0ea6a0833402b63f6b0371
petrashenko <petrashenko@samsonos.com>
2019-06-01 20:21:21 +0300

    update angular material

--- src/app/modules/material/angular-material.module.ts
+++ src/app/modules/material/angular-material.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
-import {
-  MatListModule,
-  MatInputModule,
-  MatFormFieldModule,
-  MatButtonModule,
-  MatSelectModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatSidenavModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const modules = [
   MatListModule,
