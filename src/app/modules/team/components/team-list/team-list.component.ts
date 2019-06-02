6ea30d09bc52c04c514d8d8ca5bda67e7158710b
petrashenko <petrashenko@samsonos.com>
2019-06-02 13:29:33 +0300

    added team module

---
+++ src/app/modules/team/components/team-list/team-list.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-team-list',
+  templateUrl: './team-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TeamListComponent {}
---
+++ src/app/modules/team/team.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../../shared/shared.module';
+import { TeamRoutingModule } from './team-routing.module';
+import { TeamListComponent } from './components/team-list/team-list.component';
+
+@NgModule({
+  imports: [SharedModule, TeamRoutingModule],
+  declarations: [TeamListComponent]
+})
+export class TeamModule {}
