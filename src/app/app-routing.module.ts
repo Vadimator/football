aface025b252ab21695d74df1fd6357492bf5be2
vadimp <Jx7X95nAW1>
2019-06-12 15:12:37 +0200

    added link for small size

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,38 +5,39 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MainComponent,
-    children: [
-      {
-        path: 'match',
-        loadChildren: () => import('./modules/match/match.module').then(m => m.MatchModule)
-      },
-      {
-        path: 'player',
-        loadChildren: () => import('./modules/player/player.module').then(m => m.PlayerModule)
-      },
-      {
-        path: 'field',
-        loadChildren: () => import('./modules/field/field.module').then(m => m.FieldModule)
-      },
-      {
+    {
         path: '',
-        redirectTo: '/match',
-        pathMatch: 'full'
-      }
-    ]
-  },
-  {
-    path: 'admin',
-    loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)
-  },
-  { path: '**', component: PageNotFoundComponent }
+        component: MainComponent,
+        children: [
+            {
+                path: 'match',
+                loadChildren: () => import('./modules/match/match.module').then(m => m.MatchModule)
+            },
+            {
+                path: 'player',
+                loadChildren: () => import('./modules/player/player.module').then(m => m.PlayerModule)
+            },
+            {
+                path: 'field',
+                loadChildren: () => import('./modules/field/field.module').then(m => m.FieldModule)
+            },
+            {
+                path: '',
+                redirectTo: '/match',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: 'admin',
+        loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)
+    },
+    { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
