733616b52391a56c8ac0b08bb8be5d1be89f6f54
vadimp <Jx7X95nAW1>
2019-06-20 16:33:01 +0200

    added view for player list item

--- src/app/modules/player/component/player-list-item/player-list-item.component.ts
+++ src/app/modules/player/component/player-list-item/player-list-item.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'app-player-list-item',
   templateUrl: 'player-list-item.component.html',
+  styleUrls: ['player-list-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlayerListItemComponent {
