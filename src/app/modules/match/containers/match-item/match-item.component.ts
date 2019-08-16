3c481964e871c607277ec8c63ea955a53e0a0774
vadimp <Jx7X95nAW1>
2019-08-16 17:45:46 +0200

    added store store for selected match item

--- src/app/modules/match/containers/match-item/match-item.component.ts
+++ src/app/modules/match/containers/match-item/match-item.component.ts
@@ -1,18 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { MatchFacade } from '../../store/match.facade';
 
 @Component({
-  selector: 'app-match-item',
-  templateUrl: 'match-item.component.html',
-  styleUrls: ['match-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-match-item',
+    templateUrl: 'match-item.component.html',
+    styleUrls: ['match-item.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MatchItemComponent {
-  public match$: Observable<any>;
+export class MatchItemComponent implements OnInit {
+    public match$: Observable<any>;
+    public isLoading$: Observable<boolean>;
 
-  constructor(private matchFacade: MatchFacade) {
-    this.match$ = this.matchFacade.selectedEntity$;
-  }
+    constructor(private matchFacade: MatchFacade, private route: ActivatedRoute) {
+        this.match$ = this.matchFacade.selectedEntity$;
+        this.isLoading$ = this.matchFacade.isLoading$;
+    }
+
+    ngOnInit(): void {
+        const matchId: number = +this.route.snapshot.params.id;
+        this.matchFacade.loadSelectedEntity(matchId);
+    }
 }
--- src/app/modules/match/math-routing.module.ts
+++ src/app/modules/match/math-routing.module.ts
@@ -3,20 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MatchListComponent } from './containers/match-list/match-list.component';
 import { MatchItemComponent } from './containers/match-item/match-item.component';
-import { MatchGuard } from './guards/match.guard';
 
 const routes: Routes = [
   { path: '', component: MatchListComponent },
   {
     path: ':id',
-    component: MatchItemComponent,
-    canActivate: [MatchGuard]
+    component: MatchItemComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-  providers: [MatchGuard]
+  exports: [RouterModule]
 })
 export class MatchRoutingModule {}
